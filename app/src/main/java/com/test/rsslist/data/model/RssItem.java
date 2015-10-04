9edcd0e4153f80e4ab40f97ed15739b1c41516de
drstranges <drstranges@gmail.com>
2015-10-05 00:24:12 +0300

    Implement onBind in RssAdapter.ViewHolder

--- app/src/main/java/com/test/rsslist/data/model/RssItem.java
+++ app/src/main/java/com/test/rsslist/data/model/RssItem.java
@@ -1,12 +1,77 @@
 package com.test.rsslist.data.model;
 
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created on 04.10.2015.
  */
+
+
+@Root(name = "item")
 public class RssItem {
-    public String title;
-    public String date;
-    public String description;
-    public String author;
-    public String link;
+    private static final SimpleDateFormat PUB_DATE_FORMAT =
+            new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.ENGLISH);
+    private static final String IMG_REGEX = "<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>";
+    private static final Pattern IMG_PATTERN = Pattern.compile(IMG_REGEX);
+
+    @Element(name = "title", data = true)
+    private String title;
+
+    @Element(name = "link")
+    private String link;
+
+    @Element(name = "pubDate")
+    private String date;
+
+    @Element(name = "author")
+    private String author;
+
+    @Element(name = "description", data = true)
+    private String description;
+
+    @Element(required = false)
+    private String picture;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public Date getDate() throws ParseException {
+        return PUB_DATE_FORMAT.parse(date);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPicture() {
+        if (picture == null){
+            picture = getPictureFromDescription();
+        }
+        return picture;
+    }
+
+    private String getPictureFromDescription() {
+        if (description != null) {
+            Matcher matcher = IMG_PATTERN.matcher(description);
+            if (matcher.find()) {
+                return matcher.group(1);
+            }
+        }
+        return "";
+    }
 }
--- app/src/main/java/com/test/rsslist/ui/adapter/RssAdapter.java
+++ app/src/main/java/com/test/rsslist/ui/adapter/RssAdapter.java
@@ -7,15 +7,23 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.test.rsslist.R;
 import com.test.rsslist.data.model.RssItem;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created on 04.10.2015.
  */
 public class RssAdapter extends RecyclerView.Adapter<RssAdapter.ViewHolder> {
+
+    private static final DateFormat DATE_FORMAT =
+            SimpleDateFormat.getDateTimeInstance();
 
     private final List<RssItem> mData;
 
@@ -26,7 +34,7 @@ public class RssAdapter extends RecyclerView.Adapter<RssAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                                  .inflate(R.layout.item_rss, parent, false);
+                .inflate(R.layout.item_rss, parent, false);
         ViewHolder vh = new ViewHolder(view);
         return vh;
     }
@@ -55,8 +63,30 @@ public class RssAdapter extends RecyclerView.Adapter<RssAdapter.ViewHolder> {
             mAuthor = (TextView) itemView.findViewById(R.id.text_author);
         }
 
-        public void onBind(RssItem item){
-            //TODO: Implement onBind
+        public void onBind(RssItem item) {
+            mTitle.setText(item.getTitle());
+            mAuthor.setText(item.getAuthor());
+
+            Date date = null;
+            try {
+                date = item.getDate();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            mDate.setText(formatDate(date));
+
+            String picture = item.getPicture();
+            Glide.with(itemView.getContext())
+                    .load(picture)
+                    .placeholder(android.R.drawable.gallery_thumb)
+                    .crossFade()
+                    .centerCrop()
+                    .into(mImageView);
+        }
+
+        private String formatDate(Date date) {
+            return date == null ? null : DATE_FORMAT.format(date);
         }
     }
 }
